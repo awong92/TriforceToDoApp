4e1429ab2dc1f6827c3d0cc9600cdb88cf10605d
mrw92391@gmail.com <mrw92391@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-03-01 01:04:49 +0000

    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@116 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/TriforceMain.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/TriforceMain.java
@@ -8,13 +8,33 @@ package edu.gatech.cs2340.triforce;
 import edu.gatech.cs2340.r.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class TriforceMain extends Activity {
+
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main); 	// loads the main view into the app when
-										// its opened
-	}
-}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main); //loads the main view into the app when its opened 
+        
+        Button loginButton = (Button) this.findViewById(R.id.login);
+        loginButton.setOnClickListener(new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            finish();
+          }
+        });
+        
+        Button registerButton = (Button) this.findViewById(R.id.register);
+        registerButton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View w){
+        		finish();
+        	}
+        });
+        }
+    }
+

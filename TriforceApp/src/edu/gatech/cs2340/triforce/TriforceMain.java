c92c2e4add25e421e1f2482c2169293f247d3df3
mrw92391@gmail.com <mrw92391@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-03-01 03:10:46 +0000

    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@121 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/TriforceMain.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/TriforceMain.java
@@ -9,36 +9,32 @@ import edu.gatech.cs2340.r.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-//import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Toast;
 
+//import android.widget.Toast;
 
 public class TriforceMain extends Activity {
 
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main); //loads the main view into the app when its opened 
-        
-        
-        
-        
-        Button loginButton = (Button) this.findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            finish();
-          }
-        });
-        
-        Button registerButton = (Button) this.findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new OnClickListener(){
-        	public void onClick(View w){
-        		finish();
-        	}
-        });
-        }
-    }
+	Button loginButton;
+	EditText loginText;
 
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main); // loads the main view into the app when
+										// its opened
+	}
+
+	public void loginButton(View v) {
+		Toast.makeText(this, "ENTER!", Toast.LENGTH_LONG).show();
+		
+	}
+
+	public void registerButton(View v) {
+		Toast.makeText(this, "REGISTER!", Toast.LENGTH_LONG).show();
+	}
+	
+}

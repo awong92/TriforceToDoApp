ec6a736dd99f0ebb8b14db3f773117cd65a1c3f5
wongalex92@gmail.com <wongalex92@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-03-04 09:55:04 +0000

    added new SQLiteDB class. still in the works.
    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@136 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

---
+++ TriforceApp/src/edu/gatech/cs2340/triforce/SQLiteDB.java
@@ -0,0 +1,45 @@
+package edu.gatech.cs2340.triforce;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteDB {
+
+	public static final String KEY_ROWID = "_id";
+	public static final String KEY_NAME = "users_name";
+	public static final String KEY_PASSWORD = "users_pw";
+
+	private static final String DATABASE_NAME = "UserManagerdb";
+	private static final String DATABASE_TABLE = "userTable";
+	private static final int DATABASE_VERSION = 1;
+
+	private DbHelper ourHelper;
+	private final Context ourContext;
+	private SQLiteDatabase ourDatabase;
+
+	private static class DbHelper extends SQLiteOpenHelper {
+
+		public DbHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	public SQLiteDB(Context c) {
+		ourContext = c;
+	}
+
+}

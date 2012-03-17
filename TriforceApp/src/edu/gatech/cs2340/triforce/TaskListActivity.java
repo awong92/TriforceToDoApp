1fd996f7712d35c0785aa7a76dba8c3bfad354e5
wongalex92@gmail.com <wongalex92@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-03-17 04:39:37 +0000

    trying to set up a filter dialog
    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@203 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/TaskListActivity.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/TaskListActivity.java
@@ -3,11 +3,15 @@ package edu.gatech.cs2340.triforce;
 import java.util.List;
 
 import edu.gatech.cs2340.r.R;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -26,10 +30,17 @@ public class TaskListActivity extends ListActivity implements OnClickListener {
 
 	ArrayAdapter<Task> listAdapter;
 	ImageButton newTaskButton, editTaskButton;
-	Button logoutButton;
+	Button logoutButton, filterTasksButton;
 	String filterBy = "All";
 	Spinner typeSpinner;
 	List<Task> list;
+
+	AlertDialog.Builder builder;
+	AlertDialog alertDialog;
+
+	Context mContext;
+	LayoutInflater inflater;
+	View layout;
 
 	/**
 	 * Called when the activity is first created
@@ -37,6 +48,12 @@ public class TaskListActivity extends ListActivity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_tasklist);
+
+		mContext = getApplicationContext();
+		inflater = (LayoutInflater) mContext
+				.getSystemService(LAYOUT_INFLATER_SERVICE);
+		layout = inflater.inflate(R.layout.filter_dialog,
+				(ViewGroup) findViewById(R.id.layout_filter_dialog));
 
 		typeSpinner = (Spinner) findViewById(R.id.typeOfTask);
 		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter
@@ -47,14 +64,18 @@ public class TaskListActivity extends ListActivity implements OnClickListener {
 		typeSpinner.setAdapter(spinnerAdapter);
 
 		newTaskButton = (ImageButton) findViewById(R.id.newTaskButton);
-		editTaskButton = (ImageButton) findViewById(R.id.edit_row_task);
 		logoutButton = (Button) findViewById(R.id.logoutButton);
+		filterTasksButton = (Button) findViewById(R.id.filterTasksButton);
 
 		typeSpinner.setOnItemSelectedListener(new TypeSelectedListener());
 		newTaskButton.setOnClickListener(this);
-		editTaskButton = (ImageButton) findViewById(R.id.edit_row_task);
 		logoutButton.setOnClickListener(this);
+		filterTasksButton.setOnClickListener(this);
 
+		builder = new AlertDialog.Builder(mContext); 
+		builder.setView(layout);
+		alertDialog = builder.create();
+		
 		getModel();
 		listAdapter = new ListArrayAdapter(this, list);
 		setListAdapter(listAdapter);
@@ -95,13 +116,12 @@ public class TaskListActivity extends ListActivity implements OnClickListener {
 					"edu.gatech.cs2340.triforce.NEWTASKACTIVITY");
 			startActivity(createNewTask);
 			break;
-		case R.id.edit_row_task:
-			Intent editTask = new Intent("edu.gatech.cs2340.triforce.EDITTASKACTIVITY");
-			startActivity(editTask);
-			break;
 		case R.id.logoutButton:
 			TriforceMain.currentUser = null;
 			finish();
+			break;
+		case R.id.filterTasksButton:
+			alertDialog.show();
 			break;
 		}
 	}

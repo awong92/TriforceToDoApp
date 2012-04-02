5e896525b3cb32f04706be394710ad8d084093cf
nathanepp21@gmail.com <nathanepp21@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-04-02 16:11:28 +0000

    updated to zoom in on certain lat, lng which im trying to put zoomed in on atl
    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@234 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/gmap.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/gmap.java
@@ -5,21 +5,47 @@ import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MapView.LayoutParams;
 
 public class gmap extends MapActivity {
 
 	MapView mapView;
-
+	MapController mc;
+	GeoPoint p;
+	  
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gmap);
 
 		mapView = (MapView) findViewById(R.id.mapview);
-		mapView.setBuiltInZoomControls(true);
+        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
+        View zoomView = mapView.getZoomControls(); 
+ 
+        zoomLayout.addView(zoomView, 
+            new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT)); 
+        mapView.displayZoomControls(true);
+        
+        mc = mapView.getController();
+        //atlanta lat and lng found on google lol 
+        String coordinates[] = {"20.65","240.42"};
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+        
+        p = new GeoPoint(
+        		(int)(lat *1E6), 
+        		(int)(lng*1E6));
+        
+        mc.animateTo(p);
+        mc.setZoom(3);
+        mapView.invalidate();
+	
 	}
 
 	@Override

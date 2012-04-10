50d1dbcf22e8345dde0bd672bd73886478e789c7
nathanepp21@gmail.com <nathanepp21@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-04-10 16:12:39 +0000

    Updated to run more Junit tests
    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@273 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/TaskValidation.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/TaskValidation.java
@@ -1,75 +1,115 @@
 package edu.gatech.cs2340.triforce;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.test.suitebuilder.annotation.Smoke;
+
 import com.jayway.android.robotium.solo.Solo;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.ViewAsserts;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.view.KeyEvent;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import edu.gatech.cs2340.r.R;
-import edu.gatech.cs2340.triforce.TaskListActivity;
-
-public class TaskValidation extends ActivityInstrumentationTestCase2<TaskListActivity>{
-	private String mSelection;
-	private Button testButton;
-	private Button testButton2; 
-	private TaskListActivity tlActivity;
-	private TaskListActivity mActivity;
-	private Spinner mSpinner;
+/**
+ * 
+ * @author Nathan Eppinger, Mallory Wynn, Alex Wong
+ *
+ * This class test the TaskListActivity page and connected pages 
+ * that dal with task creation, deletion, and editing. 
+ */
+public class TaskValidation extends ActivityInstrumentationTestCase2<TriforceMain>{
 	private Solo solo;
 	
 	@SuppressWarnings("deprecation")
 	public TaskValidation() {
-		super(TaskListActivity.class);
+		super("edu.gatech.cs2340.triforce",TriforceMain.class);
 	}
 
 	protected void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(),getActivity());
 	}
+	/**
+	 * tests the taskList seeing if it can add a task
+	 */
 	
-	@SmallTest
-	public void testViewsCreated(){
-		solo.assertCurrentActivity("Expected Task List", "TaskListActivity");
+	public void testButtons(){
 		solo.clickOnMenuItem("New Task");
-		//check to see if activity is opened
-		solo.assertCurrentActivity("Expected New Task Page", "NewTaskActivity");
+		solo.assertCurrentActivity("wrong page", NewTaskActivity.class);
+		solo.goBackToActivity("TaskListActivity");
+	}
+	
+	public void testAddTaskFail(){
 		solo.enterText(0, "nepp");
 		solo.enterText(1, "nepp");
-		solo.enterText(2, "6017 Leigh Read Rd.");
+		solo.clickOnButton(0);
+	//	assertEquals("fail",false,true);
+		solo.clickOnMenuItem("New Task");
+		solo.enterText(0, "elf");
+		solo.enterText(1, "Tallahassee");
+		solo.enterText(2, "6017 Leigh Read Rd. Tallahssee FL.");
 		solo.clickOnButton(3);
-		solo.clickOnMenuItem("Show Map");
-		solo.assertCurrentActivity("Expected Google maps Page", "gMaps");
-		boolean expected = true;
-		boolean actual = solo.searchText("nepp");
+		boolean expected = false;
+		boolean actual = solo.searchText("elf");
 		assertEquals("Task wasn't created", expected,actual);
 	}
-	@SmallTest
-	public void testViewsVisible(){
+	
+	/*
+	public void testAddTask(){
+		solo.assertCurrentActivity("Expected login page", "TriforceMain");
+		solo.enterText(0, "nepp");
+		solo.enterText(1, "nepp");
+		solo.clickOnButton(0);
+		
+		solo.clickOnMenuItem("New Task");
+		solo.enterText(0, "home");
+		solo.enterText(1, "Tallahassee");
+		solo.enterText(2, "6017 Leigh Read Rd. Tallahssee FL.");
+		solo.clickOnButton(2);
+		boolean expected = true;
+		boolean actual = solo.searchText("home");
+		solo.goBackToActivity("TaskListActivity");
+		assertEquals("Task wasn't created", expected,actual);
+	}
+	*/
+	/**
+	 * Tests to see if editing tasks is working
+	 */
+	/*
+	
+	public void testEditTask(){
+		solo.clickOnText("home");
+	//	solo.clickOnScreen(5, 150);
+		solo.clickOnMenuItem("Edit Task");
+		solo.enterText(0, "home2");
+		solo.clickOnButton(2);
+		assertEquals("Task Wasn't Edited", true,solo.searchText("home2"));
 		
 	}
 	
+	public void testRemoveTask(){
+		solo.clickInList(2);
+		//solo.clickLongOnScreen(5000, 5000);
+		solo.clickOnMenuItem("Delete Task");
+		assertEquals("Task wasn't deleted", false,solo.searchText("home2"));
+	}
+	*/
+	@SmallTest
+	public void addTask2(){
+		solo.clickOnMenuItem("New Task");
+		solo.enterText(0, "school");
+		solo.enterText(1, "GaTech");
+		solo.enterText(2, "Georgia Institute of Technology");
+		solo.pressSpinnerItem(0, 1);
+		solo.clickOnButton(2);
+		boolean expected = true;
+		boolean actual = solo.searchText("school");
+		assertEquals("Task wasn't created", expected,actual);
+	}
 	
-	//@SmallTest
-//	public void testViewsVisible(){
-		//ViewAsserts.assertOnScreen(mSpinner.getRootView(),mSpinner);
-	//}
 	
-	//@SmallTest
-//	public void testButtonUI(){
-	//	tlActivity.runOnUiThread(
-		//		new Runnable(){
-			//		public void run(){
-				//		mSpinner.requestFocus();
-				//}
-			//}
-		//);
-		//this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
-		//String resultText = mSpinner
-		//assertEquals(resultText,"edu.gatech.cs2340.taskListActivity");
-	//}
+	public void testGoogleM(){
+		solo.clickOnMenuItem("Show Locations");
+		assertEquals("Google Maps didn't open","gMap", solo.getCurrentActivity());
+	}
 	
-
+	@Override
+	public void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		solo.finishOpenedActivities();
+	}
 }

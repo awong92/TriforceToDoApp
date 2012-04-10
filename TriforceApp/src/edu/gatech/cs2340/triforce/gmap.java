a596b86c4e74d9ee964c1a973613f1ac234f0e73
nathanepp21@gmail.com <nathanepp21@gmail.com@4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5>
2012-04-10 03:34:16 +0000

    fixed Gmaps only displays legit locations need to fix zoom sometime
    git-svn-id: https://team-triforce-36-project.googlecode.com/svn/trunk@261 4b79e4dc-a6ce-1459-8c0c-0c5eed390bd5

--- TriforceApp/src/edu/gatech/cs2340/triforce/gmap.java
+++ TriforceApp/src/edu/gatech/cs2340/triforce/gmap.java
@@ -24,43 +24,43 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.MapView.LayoutParams;
 
-
 public class gmap<T> extends MapActivity {
 
 	MapView mapView;
 	MapController mc;
-	GeoPoint p;
+	GeoPoint p, y;
 	SQLiteDB db = new SQLiteDB(this);
 	Context c = this;
 	List<Task> tasks;
 	Geocoder geocoder;
-	double [] latt, lnng;
-	
+	double[] latt, lnng;
+	int z;
+
 	int x;
-	
-	class MapOverlay extends com.google.android.maps.Overlay
-	{
-		@Override 
-		public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when)
-		{
+
+	class MapOverlay extends com.google.android.maps.Overlay {
+		@Override
+		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
+				long when) {
 			super.draw(canvas, mapView, shadow);
-			
-			//transalting geo Point to pixels
+
+			// transalting geo Point to pixels
 			Point screenPts = new Point();
-			x = latt.length;
-			for(int i = 0; i<x; i++){
-				
-				p = new GeoPoint((int)latt[i],(int)lnng[i]);
-				mapView.getProjection().toPixels(p, screenPts);
-			
-				//add the pin
-				Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.pin);
-				canvas.drawBitmap(bmp, screenPts.x, screenPts.y-30,null);
+			for (int i = 0; i < z; i++) {
+
+				y = new GeoPoint((int) latt[i], (int) lnng[i]);
+				mapView.getProjection().toPixels(y, screenPts);
+
+				// add the pin
+				Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+						R.drawable.pin);
+				canvas.drawBitmap(bmp, screenPts.x, screenPts.y - 30, null);
 			}
 			return true;
-			
+
 		}
 	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -68,66 +68,60 @@ public class gmap<T> extends MapActivity {
 		setContentView(R.layout.gmap);
 
 		mapView = (MapView) findViewById(R.id.mapview);
-        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
-        @SuppressWarnings("deprecation")
-		View zoomView = mapView.getZoomControls(); 
- 
-        zoomLayout.addView(zoomView, 
-            new LinearLayout.LayoutParams(
-                LayoutParams.WRAP_CONTENT, 
-                LayoutParams.WRAP_CONTENT)); 
-        mapView.displayZoomControls(true);
-        
-        mc = mapView.getController();
-        geocoder = new Geocoder(this, Locale.getDefault());
-        
-        //atlanta lat and lng found on google lol 
-        String coordinates[] = {"33.778","275.602"};
-        double lat = Double.parseDouble(coordinates[0]);
-        double lng = Double.parseDouble(coordinates[1]);
-        
-        db.open();
-        try {
-			tasks =  db.getUserTasks(TriforceMain.currentUser, "All", "no date filter", "Both", c);
+		LinearLayout zoomLayout = (LinearLayout) findViewById(R.id.zoom);
+		@SuppressWarnings("deprecation")
+		View zoomView = mapView.getZoomControls();
+
+		zoomLayout.addView(zoomView, new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		mapView.displayZoomControls(true);
+
+		mc = mapView.getController();
+		geocoder = new Geocoder(this, Locale.getDefault());
+
+		// atlanta lat and lng found on google lol
+		String coordinates[] = { "33.778", "84.3881" };
+		double lat = Double.parseDouble(coordinates[0]);
+		double lng = Double.parseDouble(coordinates[1]);
+
+		db.open();
+		try {
+			tasks = db.getUserTasks(TriforceMain.currentUser, "All",
+					"no date filter", "Both", c);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		x = tasks.size();
-		latt = new double [tasks.size()];
-		lnng = new double [tasks.size()];
-		
-		for(int i = 0; i<x; i++){
+		latt = new double[tasks.size()];
+		lnng = new double[tasks.size()];
+
+		z = 0;
+		for (int i = 0; i < x; i++) {
 			Task t = tasks.get(i);
-			if(t == null){}
 			try {
-				List<Address> addressList = geocoder.getFromLocationName(t.getLocation(), 5);
-				if(addressList != null && addressList.size()>0){
-					lat = (double) (addressList.get(0).getLatitude()*1000000);
-					lng = (double) (addressList.get(0).getLongitude()*1000000);
-				
-					latt[i] = lat;
-					lnng[i] = lng;
+				if (t.getLocation()!= "") {
+					List<Address> addressList = geocoder.getFromLocationName(
+							t.getLocation(), 5);
+					if (addressList != null && addressList.size() > 0) {
+						lat = (double) (addressList.get(0).getLatitude() * 1000000);
+						lng = (double) (addressList.get(0).getLongitude() * 1000000);
+
+						latt[z] = lat;
+						lnng[z] = lng;
+						z++;
+					}
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				z--;
 				e.printStackTrace();
 			}
 		}
-        p = new GeoPoint(
-        		(int)(lat*1E6), 
-        		(int)(lng*1E6));
-        
-        mc.animateTo(p);
-        mc.setZoom(16);
-        mapView.invalidate();
-        
-        MapOverlay mapOverlay = new MapOverlay();
-        List<Overlay> listofOverlays = mapView.getOverlays();
-        listofOverlays.clear();
-        listofOverlays.add(mapOverlay);
-        
-        mapView.invalidate();
+		MapOverlay mapOverlay = new MapOverlay();
+		mapView.getOverlays().add(mapOverlay);
+
+		mc.setZoom(3);
+		mapView.invalidate();
 	}
 
 	@Override
